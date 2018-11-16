295d37a3594998f9cacd0e394578fc900f8f7f00
cahlchang <kahlua.dane@gmail.com>
2018-11-16 23:08:46 +0900

    i

--- call.js
+++ call.js
@@ -1,32 +1,37 @@
+function call_test() {
+    console.log('test call');
+
+}
+
+
 $(function () {
+    $('.record').on('click', function () {
+	call_test();
+	window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+	var recognition = new webkitSpeechRecognition();
+	recognition.lang = 'ja';
 
-    window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
-    var recognition = new webkitSpeechRecognition();
-    recognition.lang = 'ja';
+	// 録音終了時トリガー
+	recognition.addEventListener('result', function(event){
+	    var text = event.results.item(0).item(0).transcript;
+	    $("#result_text").val(text);
+	}, false);
 
-    // 録音終了時トリガー
-    recognition.addEventListener('result', function(event){
-	var text = event.results.item(0).item(0).transcript;
-	$("#result_text").val(text);
-    }, false);
-
-    // 録音開始
-    function record()
-    {
-	recognition.start();
-    }
-
+	// 録音開始
+	function record()
+	{
+	    recognition.start();
+	}
+    });
 });
-
 
 $(function () {
     $('.slack-submit').on('click', function () {
-	var url = $('#webhook').val();
-
+	var url = $('#webhook').val()
 
 	$.ajax({
 	    data: 'payload=' + JSON.stringify({
-		text: 'test',
+		text: 'test23',
 		username: 'bot'
 	    }),
 	    dataType: 'json',
