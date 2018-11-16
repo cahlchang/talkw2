643ea8904378237e90b8a4da94c34000fda96d5b
cahlchang <kahlua.dane@gmail.com>
2018-11-16 22:55:06 +0900

    i

--- call.js
+++ call.js
@@ -2,16 +2,28 @@ $(function () {
     $('.slack-submit').on('click', function () {
 	var url = $('#webhook').val();
 
-        var data = {
-            channel: '#bot_test',
-            username: 'oreno-bot',
-            text: 'Hello Slack!'
-        };
+        // var data = {
+        //     channel: '#bot_test',
+        //     username: 'bot',
+        //     text: 'Hello Slack!'
+        // };
 
-        $.ajax({
-            url: url,
-            type: 'post',
-            data: data});
 
+	$.ajax({
+	    data: 'payload=' + JSON.stringify({
+		"text": 'test'
+	    }),
+	    dataType: 'json',
+	    processData: false,
+	    type: 'POST',
+	    url: url
+	})
+	    .done(function (reply) {
+		console.log("POST to Slack succeeded")
+	    })
+	    .fail(function (xhr, status, errorThrown) {
+		console.log("Error in POST to Slack: " + errorThrown.toString())
+	    })
+	
     });
 });
