3af888b62bab2a0902c1880826e1e8fcd3e1fbde
cahlchang <kahlua.dane@gmail.com>
2018-11-16 23:58:41 +0900

    i

--- call.js
+++ call.js
@@ -1,3 +1,6 @@
+var flag_speech = 0;
+var flag_push_enable = 0;
+
 function call_test(text) {
     console.log('test call');
     var url = $('#webhook').val();
@@ -26,18 +29,51 @@ function record() {
     recognition.lang = 'ja';
     recognition.interimResults = true;
     recognition.continuous = true;
+
+    recognition.onsoundstart = function() {
+	$("#status").val("認識中");
+	flag_push_enable = false;
+    };
+    
+    recognition.onnomatch = function() {
+	$("#status").val("もう一度試してください");
+	flag_push_enable = false;
+    };
+    
+    recognition.onerror = function() {
+	$("#status").val("エラー");
+        if(flag_speech == 0)
+            record();
+    };
+    
+    recognition.onsoundend = function() {
+	$("#status").val("停止中");
+	flag_push_enable = true;
+        record();
+    };
+ 
     
     recognition.addEventListener('result', function(event){
-	console.log('event end');
-	
-	var text = event.results.item(0).item(0).transcript;
-	$("#result_text").val(text);
-	call_test(text);
+	var results = event.results;
+        for (var i = event.resultIndex; i < results.length; i++) {
+            if (results[i].isFinal) {
+                // document.getElementById('result_text').innerHTML = results[i][0].transcript;
+		$("#result_text").val(results[i][0].transcript);
+                record();
+            }
+            else {
+                document.getElementById('result_text').innerHTML = "[途中経過] " + results[i][0].transcript;
+                flag_speech = 1;
+            }
+        }	
+	// var text = event.results.item(0).item(0).transcript;
+	// $("#result_text").val(text);
+	// call_test(text);
 	
     }, false);
 
     // 録音開始
-    console.log('on record');
+    flag_speech = 0;
     recognition.start();
     console.log('recording');
 }
