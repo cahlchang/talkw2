953a81e15ee21587e86d3d97ab7827884f200c60
cahlchang <kahlua.dane@gmail.com>
2018-11-17 00:34:37 +0900

    i

--- call.js
+++ call.js
@@ -8,7 +8,7 @@ function call_test(text) {
     $.ajax({
 	data: 'payload=' + JSON.stringify({
 	    // text: text,
-	    content: text,
+	    content: msg,
 	    username: 'bot'
 	}),
 	dataType: 'json',
